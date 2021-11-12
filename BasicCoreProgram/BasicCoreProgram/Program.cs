f79fbe5d19a489c373425aba50040468a07cd732
sahiba <sahiba1214@gmail.com>
2021-11-12 20:53:53 +0530

    [sahiba1214] added : HarmonicNumber

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Harmonic Number \n 5. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -26,6 +26,10 @@ namespace BasicCoreProblems
                         res.Power();
                         break;
                     case 4:
+                        HarmonicNumber Sum = new HarmonicNumber();
+                        Sum.Harmonic();
+                        break;
+                    case 5:
                     flag = false;
                      break; 
                  }
