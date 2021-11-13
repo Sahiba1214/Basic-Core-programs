ec4b872e94997eb71916b0687e563e0f70fb9a5f
sahiba <sahiba1214@gmail.com>
2021-11-14 00:34:36 +0530

    [sahiba1214] add : EvenOrOdd

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainders \n 7. Swap Two Num \n 8. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainders \n 7. Swap Two Num \n 8. Even Or Odd Nums \n 9.Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -42,6 +42,10 @@ namespace BasicCoreProblems
                         ans.Swap();
                         break;
                     case 8:
+                        EvenOrOdd num = new EvenOrOdd();
+                        num.EvenOdd();
+                        break;
+                    case 9:
                         flag = false;
                         break;
 
