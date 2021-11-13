47b9957f1eed76b50eb11793b651ee3d416efe14
sahiba <sahiba1214@gmail.com>
2021-11-14 00:21:03 +0530

    [sahiba1214] add : SwappingTwoNumbers

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainders \n 7.Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap year \n 3. Power Of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainders \n 7. Swap Two Num \n 8. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -38,6 +38,10 @@ namespace BasicCoreProblems
                         result.Division();
                         break;
                     case 7:
+                        SwappingNumbers ans = new SwappingNumbers();
+                        ans.Swap();
+                        break;
+                    case 8:
                         flag = false;
                         break;
 
