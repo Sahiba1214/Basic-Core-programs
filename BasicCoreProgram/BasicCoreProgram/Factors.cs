7c0c520345ddb471b8f5753b4fd0ccdb8422adb7
sahiba <93565881+Sahiba1214@users.noreply.github.com>
2021-11-15 09:50:03 +0530

    Update Factors.cs

--- BasicCoreProgram/BasicCoreProgram/Factors.cs
+++ BasicCoreProgram/BasicCoreProgram/Factors.cs
@@ -7,8 +7,25 @@ namespace BasicCoreProblems
 {
     public class Factorial
     {
-        public static void Factorization();
-
+        public static void Calculation();
+        {
+            Console.WriteLine("Enter the Number to calculate Prime Factorization");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i*i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    int count=0;
+                    while(num%i==0)
+                    {
+                        num/=i;
+                        count++;
+                    }
+                    Console.WriteLine($"{i}");
+                }
+            }
+            
+        }
 
     }
 }
